58e504ff798d39f04f2dd0c9d7be3be9461ca123
Matthew Lluis <mwlluis@hotmail.com>
2020-06-15 07:30:25 -0400

    could not get uglifyjs to work in my dev environment so I had to roll back to the step before (non-minified code) and create a new branch to continue on

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,17 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 
 module.exports = {
+  
   entry: './src/main.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin ({
       title: 'Ping Pong',
       template: './src/index.html',
